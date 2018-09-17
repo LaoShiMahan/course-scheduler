c94aa921b0214cad8bbfcc6009dea3cc23369716
Brodie Kelley <brodiekelley@hotmail.com>
2018-09-17 14:27:29 -0600

    Accessed data from redux in Library component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -5,10 +5,11 @@ import Action from '../action';
 
 class LibraryCourse extends Component {
     render() {
+        const { title, description } = this.props;
         return (
             <div className="library-course">
                 <div className="library-course__title-check">
-                    <label className="library-course__title">Problem Solving</label>
+                    <label className="library-course__title">{ title }</label>
                     <Icon icon="check" className="library-course__icon" />
                 </div>
                 
@@ -17,7 +18,7 @@ class LibraryCourse extends Component {
                 <Action className="library-course__action" />
                 <div className="library-course__description">
                     <label>Course Description</label>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus numquam totam, cupiditate mollitia quaerat consequuntur fuga asperiores facilis est. Similique blanditiis fugit saepe animi nisi! Placeat magni assumenda ullam fugiat.</p>
+                    <p>{ description }</p>
                 </div>
             </div>
         );
