2c9a650b380156e8f00ec1d00f07d85632f04cbb
Brodie Kelley <brodiekelley@hotmail.com>
2018-09-17 16:27:08 -0600

    Styled arrow animations

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class Arrow extends Component {
+    constructor() {
+        super()
+        this.state = {
+            status: false
+        }
+    }
+
+    toggleArrow = () => {
+        if(this.state.status) {
+            document.getElementById('arrow').classList.remove('arrow-closed');
+        } else {
+            document.getElementById('arrow').classList.add('arrow-closed');
+        }
+        this.setState(prevState => {
+            return ({ status: !prevState.status });
+        });
+    }
+
     render() {
         const { className } = this.props;
         return (
-            <div className={ `${ className } arrow` }></div>
+            <a id="arrow" onClick={ () => this.toggleArrow() } className={ `${ className } arrow` }></a>
         );
     }
 }
