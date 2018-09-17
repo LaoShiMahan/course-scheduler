9d4e9bfc26fa71677bbf5e365992e68411ae7f16
Brodie Kelley <brodiekelley@hotmail.com>
2018-09-17 15:11:15 -0600

    Added logic to toggle_enrolled reducer

--- src/components/action.js
+++ src/components/action.js
@@ -3,12 +3,12 @@ import { Icon } from './icon';
 
 class Action extends Component {
     render() {
-        const { className } = this.props;
+        const { onClick, className } = this.props;
         return (
-            <div className={ `${ className } action` }>
+            <button onClick={() => onClick() } className={ `${ className } action` }>
                 <Icon icon="plus-circle" className={ className } />
                 {/* <Icon icon="times-circle" className={ className } /> */}
-            </div>
+            </button>
         );
     }
 }
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 import { Icon } from '../icon';
 import Arrow from '../arrow';
 import Action from '../action';
 
 class LibraryCourse extends Component {
     render() {
-        const { title, description } = this.props;
+        const { id, title, description, toggleEnrolled } = this.props;
         return (
             <div className="library-course">
                 <div className="library-course__title-check">
@@ -15,7 +17,7 @@ class LibraryCourse extends Component {
                 
                 <Arrow className="library-course__arrow" />
 
-                <Action className="library-course__action" />
+                <Action onClick={ () => toggleEnrolled(id) } className="library-course__action" />
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>{ description }</p>
@@ -25,4 +27,4 @@ class LibraryCourse extends Component {
     }
 }
 
-export default LibraryCourse;
+export default connect(null, actions)(LibraryCourse);
