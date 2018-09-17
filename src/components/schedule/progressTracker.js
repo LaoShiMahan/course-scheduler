a33f3c0e6ebe99aaa72284309a0a0594def09a4b
Brodie Kelley <brodiekelley@hotmail.com>
2018-09-17 15:31:49 -0600

    Worked on functionality of adding and removing courses

--- src/components/schedule/progressTracker.js
+++ src/components/schedule/progressTracker.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class ProgressTracker extends Component {
+    calculateProgress = () => {
+        const {max, enrolled} = this.props;
+        return enrolled / max * 100;
+    }
+
     render() {
         return (
             <div className="progress-tracker">
                 <label className="progress-tracker__label">ProgressTracker</label>
                 <div className="progress-tracker__percentage">
-                    0%
+                    { this.calculateProgress() }%
                 </div>
             </div>
         );
--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -9,18 +9,20 @@ class Schedule extends Component {
         const data = this.props.courses;
 
         return data.map((course, index) => {
-            if(course.enrolled) {
+            if(course.enrolled && this.amountEnrolled !== 5) {
+                this.amountEnrolled++;
                 return <ScheduleCourse { ...course } key={ index }/>
             }
         })
     }
 
     render() {
+        this.amountEnrolled = 0;
         return (
             <div className="schedule">
                 <h1 className="schedule__title">Course Schedule</h1>
                 { this.renderCourses() }
-                <ProgressTracker />
+                <ProgressTracker max={5} enrolled={ this.amountEnrolled }/>
                 <Gradient />
             </div>
         );
