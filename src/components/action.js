b3a8bcb3ab9627399d835748dd92a192c1cf1702
Brodie Kelley <brodiekelley@hotmail.com>
2018-09-17 21:15:26 -0600

    Styled addRemove componentand added functionality

--- src/components/action.js
+++ src/components/action.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import { Icon } from './icon';
 
 class Action extends Component {
+    constructor() {
+        super()
+        this.state = {
+            status: false
+        }
+    }
+
+    handleAction = (onClick) => {
+        onClick();
+        if(this.state.status) {
+            document.getElementById("action").classList.remove("action-remove");
+        } else {
+            document.getElementById("action").classList.add("action-remove");
+        }
+        this.setState((prevState) => {
+            return ({
+                status: !prevState.status
+            })
+        })
+    }
+
     render() {
         const { onClick, className } = this.props;
         return (
-            <button onClick={() => onClick() } className={ `${ className } action` }>
-                <Icon icon="plus-circle" className={ className } />
+            <a id="action" onClick={ () => this.handleAction(onClick) } className={ `${ className } action` }>
+                {/* <Icon icon="plus-circle" className={ className } /> */}
                 {/* <Icon icon="times-circle" className={ className } /> */}
-            </button>
+            </a>
         );
     }
 }
